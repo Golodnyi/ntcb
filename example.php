c5a372be3154549e9a7b701514b1d726d242adf3
golodnyi <ochen@golodnyi.ru>
2016-05-06 15:14:12 +0700

    example.php

---
+++ example.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * User: golodnyi
+ * Date: 05.05.16
+ * Time: 12:03
+ */
+    if (strtoupper(substr(PHP_OS, 0, 3)) == 'WIN')
+    {
+        define('SLASH', '\\');
+        define('PORT', 9000);
+    }
+    else
+    {
+        define('SLASH', '/');
+        define('PORT', 9001);
+    }
+
+    require_once __DIR__ . SLASH . 'ntcb_v6.php';
+
+    try
+    {
+        $ntcb = new ntcb_v6(true);
+        $ntcb->listen('0.0.0.0', PORT);
+        $ntcb->run();
+    } catch (Exception $e)
+    {
+        echo '[' . date(DATE_W3C) . '] ' .
+            $e->getMessage() .
+            " [code " . $e->getCode() . "]\n";
+
+        file_put_contents(
+            __DIR__ . SLASH . 'ntcb.log',
+            '[' . $e->getCode() . ']' . ' ' . $e->getMessage() . "\n",
+            FILE_APPEND
+        );
+    }
